0538186fcabda7b6e7e478db4df115f8ddfff91b
suchojlisitsin <76742929+suchojlisitsin@users.noreply.github.com>
2021-12-19 14:28:20 +0200

    Delete uchilisthe_javafx directory

--- uchilisthe_javafx/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.uchilisthe_javafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.uchilisthe_javafx to javafx.fxml;
-    exports com.example.uchilisthe_javafx;
-}
