4cd21fa4c1cecc34873135d8941fe9c940fecc97
suchojlisitsin <76742929+suchojlisitsin@users.noreply.github.com>
2021-12-19 14:27:48 +0200

    Update uchilishte.sql

--- uchilishte.sql
+++ uchilishte.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE School;
 USE School;
+
 CREATE TABLE Student(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(32),
@@ -15,4 +16,22 @@ VALUES
 	("Ivan","Goshev"),
 	("Mitko","Ivanov"),
 	("Miladin","Mitkov");
+ -- 
+CREATE TABLE Teacher(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(32),
+    familyname VARCHAR(32)
+);
+INSERT INTO Teacher(name, familyname) 
+VALUES
+	("Dimiter","Goshev"),
+	("Peter","Ivanov"),
+	("Milan","Mitkov"),
+	("Georgi","Miladinov"),
+	("Atanas","Miladinov"),
+	("Teodor","Goshev"),
+	("Teodosii","Ivanov"),
+	("Simeon","Mitkov");
+ --
 SELECT * FROM Student;
+SELECT * FROM Teacher;
